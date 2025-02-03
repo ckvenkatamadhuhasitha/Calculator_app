cebcbd853110a26e961ec1bb8c73c831995ea2a2
C K VENKATA MADHU HASITHA <153090038+ckvenkatamadhuhasitha@users.noreply.github.com>
2025-02-03 13:51:19 +0530

    Update calculator_app.c

--- calculator_app.c
+++ calculator_app.c
@@ -1,41 +1,65 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="calculator_app" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/calculator_app" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/calculator_app" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-		</Compiler>
-		<Unit filename="main.c">
-			<Option compilerVar="CC" />
-		</Unit>
-		<Extensions>
-			<lib_finder disable_auto="1" />
-		</Extensions>
-	</Project>
-</CodeBlocks_project_file>
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int choice;
+    double num1, num2, result;
+
+    while (1) {
+        printf("\nCalculator Menu:\n");
+        printf("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
+        printf("5. Modulus\n6. Floor Division\n7. Power\n0. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 0) {
+            printf("Exiting...\n");
+            break;
+        }
+
+        if (choice >= 1 && choice <= 7) {
+            printf("Enter two numbers: ");
+            scanf("%lf %lf", &num1, &num2);
+        }
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                printf("Result: %.2lf\n", result);
+                break;
+            case 2:
+                result = num1 - num2;
+                printf("Result: %.2lf\n", result);
+                break;
+            case 3:
+                result = num1 * num2;
+                printf("Result: %.2lf\n", result);
+                break;
+            case 4:
+                if (num2 != 0)
+                    printf("Result: %.2lf\n", num1 / num2);
+                else
+                    printf("Error: Division by zero!\n");
+                break;
+            case 5:
+                if ((int)num2 != 0)
+                    printf("Result: %d\n", (int)num1 %  (int)num2);
+                else
+                    printf("Error: Modulus by zero!\n");
+                break;
+            case 6:
+                if (num2 != 0)
+                    printf("Result: %.0lf\n", floor(num1 / num2));
+                else
+                    printf("Error: Division by zero!\n");
+                break;
+            case 7:
+                printf("Result: %.2lf\n", pow(num1, num2));
+                break;
+            default:
+                printf("Invalid choice! Please enter a valid option.\n");
+        }
+    }
+
+    return 0;
+}
